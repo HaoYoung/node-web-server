94e9d511c2126bfb607c46fb77e8875d51cb1993
Yang, Hao <hao-yang@uiowa.edu>
2018-05-08 15:38:01 -0500

    express middleware

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+// Set up log
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// Set up maintenance
+//app.use((req, res, next) => {
+//  res.render('maintenance.hbs');
+//});
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -16,7 +36,6 @@ hbs.registerHelper('screamIt', (text) => {
 });
 
 app.get('/', (req, res) => {
-  //res.send('<h1>Hello from Express!</h1>');
   res.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to my website'
