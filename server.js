ed0a62b98d4416632538ef06876aa5a7fd7c5c95
Yang, Hao <hao-yang@uiowa.edu>
2018-05-08 15:18:56 -0500

    data templates rendering with headerbars.js

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
   //res.send('<h1>Hello from Express!</h1>');
-  res.send({
-    name: 'Hao',
-    likes: [
-      'music',
-      'work out'
-    ]
-  });
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  })
 });
 
 app.get('/about', (req, res) => {
-  res.send('About page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
 
 app.get('/bad', (req, res) => {
@@ -23,4 +35,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
